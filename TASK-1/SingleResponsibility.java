6752e0e5db61b5b70ee7dd520e885fa8baa96e6a
prottoy5 <106944019+prottoy5@users.noreply.github.com>
2023-07-14 01:59:37 -0700

    Design Pattern

---
+++ TASK-1/SingleResponsibility.java
@@ -0,0 +1,11 @@
+public class SingleResponsibility {
+     
+    public static void main(String[] args) {
+
+        User user1 = new User();
+        EmailService email1 = new EmailService();
+
+        user1.login("Prottoy", "Password");
+        email1.sendEmail("Sani ", "SADP ", "Null ");
+    }
+}
---
+++ TASK-1/User.java
@@ -0,0 +1,7 @@
+public class User {
+    
+    public void login(String userName, String password){
+
+        System.out.println("User Name : " + userName + "\n Password : " + password);
+    }
+}
