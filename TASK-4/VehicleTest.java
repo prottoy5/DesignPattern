3f51c30630337300776901c1c315a5e9ed785bd9
prottoy5 <106944019+prottoy5@users.noreply.github.com>
2023-07-28 23:12:48 -0700

    InterfaceSegration

--- TASK-4/VehicleTest.java
+++ TASK-4/VehicleTest.java
@@ -1,19 +1,25 @@
-public class VehicleTest {   //InterfaceSegration.
+public class VehicleTest {   
 
     public static void main(String[] args)
     {
         Car car = VehicleBuilder.builderCar();
-        car.setPrice(15.00);
-        car.start();
-        car.stop();
+        car.setPrice(15.00); // Car's price is : 15.0
+        car.start(); // Car's start time is : 10:00 AM
+        car.stop(); // Car's stop time is : 12:00 PM
 
 
         Aeroplane aeroplane = VehicleBuilder.buildAeroPlane();
-        aeroplane.setPrice(25.00);
-        aeroplane.start();
-        aeroplane.stop();
-        aeroplane.fly();       
+        aeroplane.setPrice(25.00); // Aeroplane's price is : 25.0
+        aeroplane.start(); // Aeroplane's start time is : 6.00 PM
+        aeroplane.stop(); // Aeroplane's stop time is : 2.00 AM
+        aeroplane.fly(); // Aeroplane's fly time is : 8.00 PM   
 
     }
 }
+
+
+
+
+
+
 
