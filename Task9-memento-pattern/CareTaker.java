6d85adea4c44efb960b46d5e187642faaccae3f2
prottoy5 <106944019+prottoy5@users.noreply.github.com>
2023-09-01 12:42:43 -0700

    memento-pattern

---
+++ Task9-memento-pattern/CareTaker.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CareTaker {
+
+    private List<Memento> mementoList = new ArrayList<Memento>();
+
+    public void add(Memento state)
+    {
+        mementoList.add(state);
+    }
+
+    public Memento get(int index)
+    {
+        return mementoList.get(index);
+    }
+
+    public void printAll() 
+    {
+        for (Memento print : mementoList) 
+        {
+            System.out.println(print.getState());
+        }
+    }
+    
+}
